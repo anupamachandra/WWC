bc6206f36cf231eb421925e33aa961aaaaf46140
Anupama Chandrasekhar <anupamac@ngvpn01-162-6.dyn.scz.us.nvidia.com>
2019-11-14 08:10:45 -0800

    Session 2 material

---
+++ WWC-CPP-2019/CPP-Templates/Session2/CodeExamples/2_adl.cpp
@@ -0,0 +1,19 @@
+#include<algorithm>
+namespace N {
+
+struct A {};
+/*void swap(A a, A b)
+{
+
+}*/
+
+}  // namespace N
+
+int main()
+{
+    using namespace std;
+    using N::A;
+    A a;
+    A b;
+    swap(a, b);
+}
